952439c2c0b2893484ba962dc2cd61568972760b
celo-ub <Bl2k2@gh>
2020-07-26 15:11:22 -0300

    Criado método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,10 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+##    print(p.cumprimentar(7)) # aceita só um argumento posicional - p já se passa como argumento
