d45691c51cc91c8cb4e9f0b7d3ec421004218462
Marcelo Miranda <marcelomiranda70@gmail.com>
2020-07-29 00:23:37 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,19 @@
 class Pessoa:
-    def __init__(self, param_nome = None, idade=35):   # se usa __init__ para criar atributos de objetos ou instancias
-        self.idade = idade     # adicionado com Alt Enter no PyCharm
-        self.nome = param_nome # usando 'param_nome' (no lugar de nome como na aula) para distinguir:
-                               # ATRIBUTO nome (self.nome) de PARAMETRO nome (param_nome)
+    def __init__(self, *filhos, nome = None, idade=35):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Arthur')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-##    print(p.cumprimentar(7)) # aceita só um argumento posicional - p já se passa como argumento
-    print(p.nome)
-    p.nome = 'Marcelo'
-    print(p.nome)
+    arthur = Pessoa(nome='Arthur')
+    marcelo = Pessoa(arthur, nome='Marcelo') # arthur filho de marcelo
+    print(Pessoa.cumprimentar(marcelo))
+    print(id(marcelo))
+    print(marcelo.cumprimentar())
+    print(marcelo.nome)
+    print(marcelo.idade)
+    for filho in marcelo.filhos:
+        print(filho.nome)
